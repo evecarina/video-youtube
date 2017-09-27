4740b40737891c991fb5dae48dc0b76c01e2c7a5
carina120794@gmail.com <you@example.com>
2017-09-27 14:46:02 -0500

    reproducion de los videos grandes, al haacer click en las imagenes

--- app.js
+++ app.js
@@ -9,29 +9,38 @@ class Youtube {
       selectedVideo: null,
       searchTerm: "iPhone X"
       };
-      /*arrow function necesario*/
       this.youtubeSearch("iPhone X");
+      /*arrow function necesario*/
       $('#video-search').click(()=>{
-         let videoAct = $('#input-data').val();
-          $("#root").empty();
-         console.log(videoAct);
-         this.youtubeSearch(videoAct);
+         let current_video = $('#input-data').val();
+        $("#root").empty();
+         console.log(current_video);
+         this.youtubeSearch(current_video);
+      });
+
+      $('div').click(()=>{
+        let current_video = $('#input-data').val();
+        this.playVideo(current_video);
       });
    }
+
    //<iframe className="embed-responsive-item" src={url}> </iframe>
    getVideoList(videos) {
      return videos.map((video, index) => {
         const imageUrl = video.snippet.thumbnails.default.url;
         const url = `https://www.youtube.com/embed/${video.id.videoId}`;
         const description=video.snippet.description;
-       return $('#root').append(`<div><iframe class="embed-responsive-item" src=${url}> </iframe>
-       <p>${description}</p></div>` )
+        const title=video.snippet.title;
+
+         return $('#root').append(`<div><img class="media-object" src=${imageUrl} />
+         <p>${title}</p></div>`)
+
 
      });
    }
+
    youtubeSearch(searchTerm) {
       console.log(searchTerm);
-
       YTSearch({ key: API_KEY, term: searchTerm }, data => {
          console.log("result", data);
          this.result = {
@@ -39,25 +48,46 @@ class Youtube {
             selectedVideo: data[0],
             searchTerm: searchTerm
          };
+         console.log('video',this.result.selectedVideo);
          let list = this.getVideoList(this.result.videos);
          console.log("lis: ", list);
          $("#root").append(list);
       });
+
    }
 
-   videoSearch(searchTerm) {
-      jQuery.getJSON("list.json", data => {
-         console.log("result", data.items);
-         this.result = {
-            videos: data.items,
-            selectedVideo: data.items[0],
-            searchTerm: searchTerm
-         };
-         var list = app.getVideoList(app.result.videos);
-         console.log("lis: ", list);
-         $("#root").append(list);
-      });
-   }
+   videoList(videos) {
+          const title=videos.snippet.title;
+          const description=videos.snippet.description;
+          const url = `https://www.youtube.com/embed/${videos.id.videoId}`;
+         return $('#div_vprincipal').html(`<iframe class="embed-responsive-item" src=${url}> </iframe><p>${title}</p><p>${description}</p>`)
+    }
+
+  playVideo(searchTerm){
+    let sour=event.target.src;
+
+    let position;
+    console.log('sour',sour);
+       this.result.videos.map((video,index)=>{
+        const imageUrl = video.snippet.thumbnails.default.url;
+         return (sour==imageUrl)? position=index: '';
+       });
+       this.videoList(this.result.videos[position]);
+}
+
+  //  videoSearch(searchTerm) {
+  //     jQuery.getJSON("list.json", data => {
+  //        console.log("result", data.items);
+  //        this.result = {
+  //           videos: data.items,
+  //           selectedVideo: data.items[0],
+  //           searchTerm: searchTerm
+  //        };
+  //        var list = app.getVideoList(app.result.videos[0]);
+  //        console.log("lis: ", list);
+  //        $("#div_vprincipal").append(list);
+  //     });
+  //  }
 }
 
 let video = new Youtube();
